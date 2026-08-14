dad7f6f688f6131e25b125ff5b3d127e4be4b8d0
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-08-15 00:41:32 +0200

    remove legacy configuration trees

--- nix/modules/sops.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  # Without this, hashedPasswordFile is only seeded at user creation
-  users.mutableUsers = false;
-
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  sops.age.generateKey = true;
-  sops.age.sshKeyPaths = [ ];
-}
