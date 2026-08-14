00e5da2806e6c3b0de43c5d20201c7b255063f7f
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-08-15 00:03:12 +0200

    add feature profile loader

---
+++ lib/load-features.nix
@@ -0,0 +1,27 @@
+{ lib }:
+
+{
+  directory,
+  args,
+  exclude ? [ "default.nix" ],
+}:
+
+let
+  entries = builtins.readDir directory;
+  featureFiles = lib.filter (
+    name:
+    entries.${name} == "regular"
+    && lib.hasSuffix ".nix" name
+    && !(builtins.elem name exclude)
+  ) (builtins.attrNames entries);
+  features = map (name: import (directory + "/${name}") args) featureFiles;
+  facetModules = facet: lib.filter (module: module != null) (map (feature: feature.${facet} or null) features);
+  facet = name:
+    let
+      imports = facetModules name;
+    in
+    lib.optionalAttrs (imports != [ ]) {
+      ${name} = { inherit imports; };
+    };
+in
+facet "nixos" // facet "homeManager" // facet "darwin"
