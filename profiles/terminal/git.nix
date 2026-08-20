db7cad87b63508dbd541c77538eb50445228e07c
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-08-20 11:52:30 +0200

    make git a module for easier consumption

--- profiles/terminal/git.nix
+++ profiles/terminal/git.nix
@@ -1,36 +1,60 @@
 {
-  homeManager = { ... }: {
-    programs.git = {
-      enable = true;
-      settings = {
-        init = {
-          defaultBranch = "master";
+  homeManager =
+    { config, lib, ... }:
+    let
+      personalIdentity = {
+        name = "BennyDeeDev";
+        email = "45900418+BennyDeeDev@users.noreply.github.com";
+      };
+      personalRepositoryInclude = repository: {
+        condition = "gitdir:${repository}";
+        contents = {
+          user = personalIdentity;
+          commit.gpgSign = false;
+          tag.gpgSign = false;
         };
-        pull = {
-          rebase = true;
+      };
+    in
+    {
+      options.my.git = {
+        identity = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          default = personalIdentity;
         };
-        user = {
-          name = "BennyDeeDev";
-          email = "45900418+BennyDeeDev@users.noreply.github.com";
+
+        personalRepositories = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+        };
+      };
+
+      config = {
+        programs.git = {
+          enable = true;
+          settings = {
+            init.defaultBranch = "master";
+            pull.rebase = true;
+            user = config.my.git.identity;
+          };
+          includes = map personalRepositoryInclude config.my.git.personalRepositories;
+        };
+
+        programs.gh = {
+          enable = true;
+          settings.git_protocol = "ssh";
+          gitCredentialHelper.enable = false;
+        };
+
+        programs.lazygit = {
+          enable = true;
+          enableZshIntegration = true;
+        };
+
+        programs.zsh.shellAliases = {
+          g = "git";
+          gcm = "git commit -m";
+          gcam = "git commit -a -m";
         };
       };
     };
-
-    programs.gh = {
-      enable = true;
-      settings.git_protocol = "ssh";
-      gitCredentialHelper.enable = false;
-    };
-
-    programs.lazygit = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    programs.zsh.shellAliases = {
-      g = "git";
-      gcm = "git commit -m";
-      gcam = "git commit -a -m";
-    };
-  };
 }
