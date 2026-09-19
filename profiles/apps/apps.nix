661659032d5e5ac75974de21aec795ed9817de4a
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-09-19 18:05:45 +0200

    fix spotify consuming up to 3 cpu cores because of xwayland

--- profiles/apps/apps.nix
+++ profiles/apps/apps.nix
@@ -2,6 +2,8 @@
   homeManager =
     { pkgs, ... }:
     {
+      home.sessionVariables.NIXOS_OZONE_WL = "1";
+
       home.packages = with pkgs; [
         spotify
         nerd-fonts.jetbrains-mono
