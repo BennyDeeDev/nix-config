496ec78be31ee45508b8165f961c5d6c822bd5a4
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-09-22 16:35:17 +0200

    enable wake on lan on gaming pc

--- hosts/desktop/host.nix
+++ hosts/desktop/host.nix
@@ -2,6 +2,7 @@
   nixos = {
     nixpkgs.hostPlatform = "x86_64-linux";
     networking.hostName = "nixos";
+    networking.interfaces.enp14s0.wakeOnLan.enable = true;
     system.stateVersion = "25.11";
   };
 }
